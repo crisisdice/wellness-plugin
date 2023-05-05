9d7c7cdfe76bfe3648dd83280983dcdb1c652c6a
Alexander Daily <alexanderdaily001@gmail.com>
2023-05-05 23:37:32 +0200

    read attach

--- src/api.rs
+++ src/api.rs
@@ -1,3 +1,10 @@
+//use std::io::Cursor;
+//use std::path::PathBuf;
+//use std::io::Result;
+
+use mail_parser::Message;
+//use zip_extract;
+
 use rhai_dylib::rhai::plugin::{
     mem, Dynamic, FnAccess, FnNamespace, ImmutableString, NativeCallContext, PluginFunction,
     RhaiResult, TypeId,
@@ -10,7 +17,16 @@ use rhai_dylib::rhai::Module;
 pub mod wellness_plugin {
     /// Printing to the console using Rust.
     #[rhai_fn(global)]
-    pub fn print_stuff(string: String) {
-        println!("{}", string);
+    //pub fn print_stuff(email: String) -> Result<()> {
+    pub fn print_stuff(email: String) {
+        let bytes = email.into_bytes();
+        let message = Message::parse(&bytes).unwrap();
+        // let attatchment = message.attachment(0).unwrap();
+        // let attatchment_bytes = attatchment.contents();
+
+        println!("{:?}", message.from());
+
+        // let target = PathBuf::from("/out");
+        // zip_extract::extract(Cursor::new(attatchment_bytes), &target, true).unwrap();
     }
 }
