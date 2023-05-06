770f87636f45aba8a74bdaf2584875fb884290cd
Alexander Daily <alexanderdaily001@gmail.com>
2023-05-06 17:37:45 +0200

    zip

--- src/api.rs
+++ src/api.rs
@@ -1,9 +1,8 @@
-//use std::io::Cursor;
-//use std::path::PathBuf;
-//use std::io::Result;
+use std::io::Cursor;
+use std::path::PathBuf;
 
 use mail_parser::Message;
-//use zip_extract;
+use zip_extract;
 
 use rhai_dylib::rhai::plugin::{
     mem, Dynamic, FnAccess, FnNamespace, ImmutableString, NativeCallContext, PluginFunction,
@@ -12,21 +11,20 @@ use rhai_dylib::rhai::plugin::{
 
 use rhai_dylib::rhai::Module;
 
+const TMP_PATH: &str = "/tmp/vsmtp";
+
 // The plugin API from rhai can be used to create your plugin API.
 #[rhai_dylib::rhai::plugin::export_module]
 pub mod wellness_plugin {
     /// Printing to the console using Rust.
     #[rhai_fn(global)]
-    //pub fn print_stuff(email: String) -> Result<()> {
     pub fn print_stuff(email: String) {
         let bytes = email.into_bytes();
         let message = Message::parse(&bytes).unwrap();
-        // let attatchment = message.attachment(0).unwrap();
-        // let attatchment_bytes = attatchment.contents();
+        let attatchment = message.attachment(0).unwrap();
+        let attatchment_bytes = attatchment.contents();
 
-        println!("{:?}", message.from());
-
-        // let target = PathBuf::from("/out");
-        // zip_extract::extract(Cursor::new(attatchment_bytes), &target, true).unwrap();
+        let target = PathBuf::from(TMP_PATH);
+        zip_extract::extract(Cursor::new(attatchment_bytes), &target, true).unwrap();
     }
 }
